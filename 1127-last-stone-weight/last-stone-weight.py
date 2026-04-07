1b6d178a51e152584187856a33232e2501617d3b
Sujay Gopal Mulagund <smulagun@asu.edu>
2026-04-07 16:07:16 -0700

    Time: 0 ms (100.00%) | Memory: 19.6 MB (20.97%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -1,16 +1,13 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        stones.sort()
+        for i, s in enumerate(stones):
+            stones[i] = -s
+        heapify(stones)
         while stones:
-            s1 = stones.pop() # the heaviset stone
+            s1 = -heappop(stones)
             if not stones:
                 return s1
-            s2 = stones.pop() # the second heaviest stone; s2<= s1
+            s2 = -heappop(stones)
             if s1>s2:
-                # We need to insert the remaining stone (s1-s2) into the list
-                for i in range(len(stones) + 1):
-                    if i == len(stones) or stones[i] >= s1-s2:
-                        stones.insert(i,s1-s2)
-                        break
-            # else s1 == s2; both are destroyed
-        return 0 # if no more stones remain
+                heappush(stones, s2-s1)
+        return 0
